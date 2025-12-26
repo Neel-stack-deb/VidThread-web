f29984387c8e198ac868add5d3238dbe21cf15c9
Debarshi Majumder <debarshimajumder.dev@gmail.com>
2025-12-26 14:55:52 +0530

    added the getComments controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -8,11 +8,46 @@ const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
+    //I need to get the 10 videos based on the page and limit
+    const comments = Comment.aggregate([
+        {
+            $match:{video:videoId}
+        },
+        {
+            $lookup:{
+                from: "User",
+                localField: "$owner",
+                foreignField: "$_id",
+                as:"user"
+            }
+        },
+        {
+            $skip:(page*limit)
+        },
+        {
+            $limit:limit
+        },
+        {
+            $project:{
+                "user.fullName":1,
+                "user.avatar":1,
+                "user.username":1,
+                "content":1,
+                "timestamp":1
+            }
+        }
+    ])
+    
+    if(!comments){
+        return new ApiError(404,"No comments found")
+    }
 
+    return new ApiResponse(200,"All the comments are retrieved",comments)
 })
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    
 })
 
 const updateComment = asyncHandler(async (req, res) => {
