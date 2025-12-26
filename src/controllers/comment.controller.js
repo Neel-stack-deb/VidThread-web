4d69b02e04701c5b5d4a3f301e6027faba354b2c
Debarshi Majumder <debarshimajumder.dev@gmail.com>
2025-12-26 15:33:19 +0530

    finished the comments controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -9,7 +9,7 @@ const getVideoComments = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
     //I need to get the 10 videos based on the page and limit
-    const comments = Comment.aggregate([
+    const comments = await Comment.aggregate([
         {
             $match:{video:videoId}
         },
@@ -39,28 +39,49 @@ const getVideoComments = asyncHandler(async (req, res) => {
     ])
     
     if(!comments){
-        return new ApiError(404,"No comments found")
+        throw new ApiError(404,"No comments found")
     }
 
-    return new ApiResponse(200,"All the comments are retrieved",comments)
+    return res.status(200).json(new ApiResponse(200,comments,"All the comments are retrieved"))
 })
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
-    
+    const ownerId = req.user._id;
+    const {content} = req.body;
+    const {videoId} = req.params;
+    const comment = await Comment.create({
+        owner: ownerId,
+        content,
+        video:videoId
+    });
+    return res.status(201).json(new ApiResponse(201,toObject(comment),"New comment added."))
 })
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    const {commentId}=req.params;
+    const matchedcomment =await Comment.findOne({_id:commentId}).lean();
+    if(!matchedcomment){
+        throw new ApiError(400,"No comment found")
+    }
+    const {content} = req.body;
+    const updatedComment = await Comment.updateOne({_id:commentId},{
+        $set:{content}
+    },{new:true});
+    return res.status(202).json(new ApiResponse(202,toObject(updatedComment),"Comment updated"))
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    const {commentId} = req.params;
+    await Comment.deleteOne({_id:commentId});
+    return res.status(200).json(new ApiResponse(200,{},"Comment deleted."))
 })
 
 export {
     getVideoComments, 
     addComment, 
     updateComment,
-     deleteComment
-    }
+    deleteComment
+}
